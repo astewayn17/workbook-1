db66c9478025499919c996c291d669f63d806748
Asteway Nigussie <asteway17@gmail.com>
2025-04-10 11:32:02 -0500

    completed ShoppingList app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Glue");
+        System.out.println("Scissors");
+        System.out.println("Tape");
+        System.out.println("Drill");
+        System.out.println("Wrench");
+        System.out.println("Hammer");
+        System.out.println("Gloves");
+        System.out.println("Knife");
+        System.out.println("Towel");
+        System.out.println("Soap");
+
+    }
+}
